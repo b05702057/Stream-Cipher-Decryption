ffb8b97aa82fe894e3f04e6989c39b9dcf1b549d
Ubuntu <ubuntu@ip-172-31-21-143.us-west-2.compute.internal>
2022-04-11 05:23:42 +0000

    ok

--- 456.c
+++ 456.c
@@ -14,7 +14,7 @@ char randchar() {
 int main(int argc, const char* argv[]) {
   static char randstate[64];
   // The size of the array affects how sophisticated a random-number generator would be.
-  initstate(1000, randstate, 31); // seed, state array, array size
+  initstate(time(NULL), randstate, 31); // seed, state array, array size
   for (int j = 0; j < 10; j++) {
       printf("%c", randchar());
   }
